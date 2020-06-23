9533567d25e851c623bb3f952ae4b9ac77720ec6
Olli Helminen <opadele@gmail.com>
2020-06-23 21:08:18 +0300

    Creating voice connection working

--- main.js
+++ main.js
@@ -1,27 +1,70 @@
 var Discord = require('discord.js');
 var config = require('./config.json');
 var user_config = require('./user_configuration.json');
+var ytdl = require('ytdl-core');
+
+const queue = {
+    voiceChannel: null,
+    songs: [],
+    volume: 5,
+    connection: null
+}
 
 
 var bot = new Discord.Client();
 bot.on('ready', () => {
     console.log('Connected');
-    let channel = bot.channels.cache.find(ch => ch.name === 'General');
-    channel.join();
 })
 
 bot.on('message', (message) => {
     if(!message.content.startsWith(config.prefix)) return;
+    if(message.author.bot) return;
     if(message.channel != 724741464604540959) return;
     let args = message.content.substring(1).split(' ');
     switch(args[0]) {
         case 'testi':
-            console.log(user_config[message.member.id].song);
-            let channel = message.guild.channels.cache.find(ch => ch.name === 'lanit');
-            channel.send(user_config[message.member.id].song);
+            const voiceChannel = message.member.voice.channel;
+            if (!voiceChannel){
+                console.log(`User: ${message.member.user.username} not in voice channel`);
+            }
+            let songInfo = ytdl.getInfo(user_config[message.member.user.id].song);
+            let song = {
+                title: songInfo.title,
+                url: songInfo.video_url
+            };
+            addSong(song, voiceChannel);
         break;
     }
 
 
 })
+
+async function addSong(song, channel) {
+    queue.songs.push(song);
+    if (!queue.connection){
+        try {
+            queue.connection = await channel.join();
+        }
+        catch(err){
+            console.log(err);
+            queue.songs = [];
+        }
+    }
+    play(queue.songs[0]);
+}
+
+function play(song) {
+    if(!song){
+        queue.voiceChannel.leave();
+        return;
+    }
+    const dispatcher = queue.connection
+        .play(ytdl(song.url))
+        .on("finish", () => {
+            queue.songs.shift();
+            play(queue.songs[0]);
+        })
+        .on("error", error => console.error(error));
+    dispatcher.setVolumeLogarithmic(queue.volume / 5);
+}
 bot.login(config.token);
